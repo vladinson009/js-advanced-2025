167682fe74ab38525e706eace67e1baf1fa733a5
vladinSon <vladinson009@icloud.com>
2025-02-05 23:01:44 +0100

    array extension

---
+++ 09.Prototypes-exercises/01.arrayExtension.js
@@ -0,0 +1,17 @@
+(function arrayExtension() {
+  Array.prototype.last = function () {
+    return this.at(this.length - 1);
+  };
+  Array.prototype.skip = function (n) {
+    return this.slice(n);
+  };
+  Array.prototype.take = function (n) {
+    return this.slice(0, n);
+  };
+  Array.prototype.sum = function () {
+    return this.reduce((acc, current) => acc + current, 0);
+  };
+  Array.prototype.average = function () {
+    return this.sum() / this.length;
+  };
+})();
